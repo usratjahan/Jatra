b4efb831f9c48248f20e69aeeb5b17c2ef70e50b
Usrat Jahan <usratjahan642@gmail.com>
2026-03-23 10:06:50 +0600

    Initial React project structure

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import "./App.css";
+import { ref } from "process";
+ref
+
+
+export default App;
