1ff9d20fe6aec7cc1b2a473acc2bf93bfaccdca6
Usrat Jahan <usratjahan642@gmail.com>
2026-03-23 11:29:19 +0600

    added main.jsx entry point and App.jsx with BrowserRouter and Home route only

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router-dom";
 import "./App.css";
-import { ref } from "process";
-ref
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+const UserLayout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+    <Footer />
+  </>
+);
 
-
-export default App;
